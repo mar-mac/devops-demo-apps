ca3f75735449ea125a8a40f2997cb6692518650e
Marco Maccio <marco.maccio@marmac.name>
2017-11-07 22:21:51 +0100

    Changed the title application

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('devops-demo-apps App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to DevOps Demo WebApp!');
   });
 });
