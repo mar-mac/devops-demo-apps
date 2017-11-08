7f5870e5d5d9cf3a56fc9ef4c32eb7e9f28e154e
Marco Maccio <marco.maccio@marmac.name>
2017-11-08 14:43:30 +0100

    Jenkins - Pipeline - Created a first version of J. Pipeline

---
+++ JenkinsFile.groovy
@@ -0,0 +1,64 @@
+pipeline {
+
+    agent any
+
+    parameters {
+      string(defaultValue: 'eu-west-1', description: 'AWS Region in which deploy this infrastructure',  name: 'AWS_REGION')
+    }
+
+    stages {
+            stage('Checkout-EnvPreparation') {
+                agent any
+                steps {
+                    echo 'Checkout-EnvPreparation stage: this will checkout the source code from github'
+
+                    git branch: 'jenkins-pipeline',
+                            credentialsId: 'github-marcomaccio',
+                            url: 'https://github.com/mar-mac/devops-demo-apps.git'
+
+
+                }
+            }
+            stage('Dependency-Installation') {
+                agent any
+                steps {
+
+                    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
+                      echo 'Dependency installation via yarn ...'
+                      sh "yarn install"
+                    }
+                }
+            }
+            stage('FE-Unit-Testing') {
+                agent any
+                steps {
+
+                    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
+                      echo 'Launching unit tests via yarn ...'
+                      sh "ng test"
+                    }
+                }
+            }
+            stage('FE-E2E-Testing') {
+                agent any
+                steps {
+
+                    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
+                      echo 'Launching unit tests via yarn ...'
+                      sh "ng e2e"
+                    }
+                }
+            }
+            stage('FE-Build-Prod-And-Deploy-AWS') {
+                agent any
+                steps {
+
+                    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
+                      echo 'Launching unit tests via yarn ...'
+                      sh "yarn deploy"
+                    }
+                }
+            }
+    }
+
+}
